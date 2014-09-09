7a7ae0b2688a950217746e5ef69dcaa8a84b9420
Benjamin Brown <ben.brown@factmint.com>
2014-09-09 12:15:38 +0100

    Initial commit

---
+++ dist/axis.js
@@ -0,0 +1,94 @@
+Snap.plugin(function(Snap, Element, Paper) {
+
+	var PADDING = 10;
+	var VERTICAL_TICK_PADDING = 10;
+
+	var AXIS_LABEL_FONT_FAMILY = "'Lato', sans-serif";
+	var AXIS_LABEL_FONT_WEIGHT = 400;
+	var AXIS_LABEL_FONT_SIZE = 12;
+	var AXIS_LABEL_PADDING = 5;
+
+	Paper.prototype.axis = function(startX, startY, scale, tickMarkValues, tickMarkSize, orientation, label) {
+		var paper = this;
+		var axis = paper.g();
+
+		var tickMarks = this.g();
+		var totalValues = tickMarks.length;
+
+		axis.startPoints = [];
+
+		if (orientation === 'horizontal') {
+			tickMarkValues.forEach(function(tickMark) {
+				var pixel = scale.getPixel(tickMark);
+				axis.startPoints.push({
+					x: pixel,
+					y: startY
+				});
+				tickMarks.append(
+					paper.tickMark(
+						pixel,
+						startY,
+						'vertical',
+						tickMarkSize,
+						tickMark,
+						VERTICAL_TICK_PADDING
+					)
+				);
+			});
+
+			var axisLine = paper.line(startX, startY, scale.end, startY)
+				.addClass('fm-axis');
+
+			var axisLabel = paper.text(scale.middle, tickMarks.getBBox().y + tickMarks.getBBox().height + PADDING * 1.75, label)
+			axisLabel.attr({
+				'font-family': AXIS_LABEL_FONT_FAMILY,
+				'font-weight': AXIS_LABEL_FONT_WEIGHT,
+				'font-size': AXIS_LABEL_FONT_SIZE,
+				'text-anchor': 'middle'
+			});
+
+			axis.append(axisLine);
+			axis.append(axisLabel);
+			axis.append(tickMarks);
+		} else {
+			tickMarkValues.forEach(function(tickMark, tickMarkIndex) {
+				var pixel = scale.getPixel(tickMark);
+	
+				axis.startPoints.push({
+					x: startX,
+					y: pixel
+				});
+				var newTickMark = paper.tickMark(
+					startX,
+					pixel,
+					'horizontal',
+					tickMarkSize,
+					tickMark
+				);
+				tickMarks.append(newTickMark);
+			});
+			axis.append(tickMarks);
+
+			var axisBBox = axis.getBBox();
+
+			var axisLabel = paper.text(
+				axisBBox.x - (AXIS_LABEL_FONT_SIZE / 2) - AXIS_LABEL_PADDING,
+				scale.middle,
+				label
+			);
+
+			var axisLabelBBox = axisLabel.getBBox();
+			axisLabel.attr({
+				'font-family': AXIS_LABEL_FONT_FAMILY,
+				'font-weight': AXIS_LABEL_FONT_WEIGHT,
+				'font-size': AXIS_LABEL_FONT_SIZE,
+				'text-anchor': 'middle'
+			})
+				.transform('r -90');
+
+			axis.append(axisLabel);
+		}
+
+		return axis;
+	};
+});
